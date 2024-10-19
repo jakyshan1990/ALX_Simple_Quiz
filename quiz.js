6a93ab3c7c9a8b0af77d81b1fb2314cc029c9381
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-20 02:40:41 +0300

    okay122a1q

--- quiz.js
+++ quiz.js
@@ -1,13 +1,22 @@
+let btn1 = document.getElementById ('submit-answer');
+
+btn1.addEventListener ('click', checkAnswer);
+
+
 function checkAnswer(){
+
 let correctAnswer="4";userAnswer="0";
+
 userAnswer=document.querySelector('input[name=quiz]:checked').value;
-console.log(userAnswer);
+
+
 if( userAnswer === correctAnswer ){
     document.getElementById("feedback").innerHTML="Correct! Well done.";
-}else{
+}
+
+else{
    document.getElementById ('feedback').innerHTML = "That's incorrect. Try again!";
  
 }
+
 }
-let btn1=document.getElementById("submit-answer");
-btn1.addEventListener("click",checkAnswer);
