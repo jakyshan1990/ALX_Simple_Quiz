1209eb2bafb62dfbffc19aab337b8ef44f9d694b
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-19 23:28:08 +0300

    okay243

--- quiz.js
+++ quiz.js
@@ -1,1 +1,10 @@
-
+function checkAnswer(){
+let correctAnswer=4;userAnswer=0;
+userAnswer=document.querySelector('input[name=quiz]:checked').value;
+console.log(userAnswer);
+if(userAnswer==correctAnswer){
+    document.getElementById("feedback").innerHTML="Correct! Well done.";
+}
+}
+let btn1=document.getElementById("submit-answer");
+btn1.addEventListener("submit",checkAnswer());
