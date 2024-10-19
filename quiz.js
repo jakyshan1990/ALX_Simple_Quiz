39e9c56e50d9fdc8d8e5dfb5ef9612a8c00907ba
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-20 02:18:33 +0300

    okay122

--- quiz.js
+++ quiz.js
@@ -4,7 +4,10 @@ userAnswer=document.querySelector('input[name=quiz]:checked').value;
 console.log(userAnswer);
 if(userAnswer==correctAnswer){
     document.getElementById("feedback").innerHTML="Correct! Well done.";
+}else{
+   document.getElementById ('feedback').innerHTML = "That's incorrect. Try again!";
+ 
 }
 }
 let btn1=document.getElementById("submit-answer");
-btn1.addEventListener("submit",checkAnswer());
+btn1.addEventListener("click",checkAnswer);
