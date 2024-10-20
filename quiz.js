5e3211d793f009d50f12d2e0d22db0e5dbe452a5
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-20 03:20:38 +0300

    oqid

--- quiz.js
+++ quiz.js
@@ -12,10 +12,10 @@ userAnswer=document.querySelector('input[name=quiz]:checked').value;
 
 
 if(userAnswer === correctAnswer){
-    document.getElementById("feedback").innerHTML="Correct! Well done.";
+    document.getElementById("feedback").textContent="Correct! Well done.";
 }
 else {
-   document.getElementById ('feedback').innerHTML = "That's incorrect. Try again!";
+   document.getElementById ('feedback').textContent = "That's incorrect. Try again!";
  
 }
 
