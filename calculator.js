d6999d9e4b77d871933bc9a8724d9ac3dafeab39
jakyshan1990 <jakyshan1990@yahoo.com>
2024-10-23 01:12:15 +0300

    neww2

--- calculator.js
+++ calculator.js
@@ -1,13 +1,52 @@
- const one=parseFloat(document.getElementById("number1").value)||0; 
- const two=parseFloat(document.getElementById("number2").value)||0;
+
+
 
 function add (numbr1, numbr2) {
-  return numbr1 + numbr2;
+  return (numbr1 + numbr2);
+}
+
+function sub(numbr1,numbr2){
+  return(numbr1-numbr2);
+}
+ 
+function mul (numbr1, numbr2) {
+  return numbr1*numbr2;
+}
+
+function divide (numbr1, numbr2) {
+  return numbr1/numbr2;
 }
 
 
 document.getElementById("add").addEventListener('click',function(){
+ const one = parseFloat (document.getElementById ('number1').value) || 0;
+const two = parseFloat (document.getElementById ('number2').value) || 0;
+
   const result = add (one, two);
 document.getElementById("calculation-result").textContent=result;
 });
 
+document.getElementById ('subtract').addEventListener ('click', function () {
+  const one = parseFloat (document.getElementById ('number1').value) || 0;
+  const two = parseFloat (document.getElementById ('number2').value) || 0;
+
+  const result = sub (one, two);
+  document.getElementById ('calculation-result').textContent = result;
+});
+
+document.getElementById ('multiply').addEventListener ('click', function () {
+  const one = parseFloat (document.getElementById ('number1').value) || 0;
+  const two = parseFloat (document.getElementById ('number2').value) || 0;
+
+  const result = mul (one, two);
+  document.getElementById ('calculation-result').textContent = result;
+});
+
+
+document.getElementById ('divide').addEventListener ('click', function () {
+  const one = parseFloat (document.getElementById ('number1').value) || 0;
+  const two = parseFloat (document.getElementById ('number2').value) || 0;
+
+  const result = divide (one, two);
+  document.getElementById ('calculation-result').textContent = result;
+});
